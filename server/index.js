89ec1d84251a32a7532dd481e488247058202839
GreysonFlippo1 <greysonflippo@gmail.com>
2024-09-07 15:08:57 -0700

    changed some default text

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const app = express()
 //user config
 const config = require('./config.json')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Server is running'))
 
 app.use('/library', express.static(path.join(__dirname, config.libraryDirectories[0])))
 
-app.listen(3000, () => console.log('listening on port 3000!'))
+app.listen(3000, () => console.log('listening on port 3000'))
