c197d0cab43002e566446255b9fce127dff79a2b
GreysonFlippo1 <greysonflippo@gmail.com>
2024-09-07 15:06:47 -0700

    Added basic static file serving

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+
+//user config
+const config = require('./config.json')
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use('/library', express.static(path.join(__dirname, config.libraryDirectories[0])))
 
+app.listen(3000, () => console.log('listening on port 3000!'))
