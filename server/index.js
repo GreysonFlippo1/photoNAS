8c2b000773c431f6b41149013a389900383ea7e0
GreysonFlippo1 <greysonflippo@gmail.com>
2024-09-07 15:24:55 -0700

    serve library paths by library name

--- server/index.js
+++ server/index.js
@@ -7,6 +7,16 @@ const config = require('./config.json')
 
 app.get('/', (req, res) => res.send('Server is running'))
 
-app.use('/library', express.static(path.join(__dirname, config.libraryDirectories[0])))
+const defaultLibrary = config.libraryDirectories.find(dir => dir.isDefault)
+
+app.use('/library/:libraryName/:file', (req, res) => {
+    const searchedLibrary = config.libraryDirectories.find(dir => dir.name === req.params.libraryName)
+    if (searchedLibrary) {
+        res.sendFile(path.join(__dirname, searchedLibrary.path, req.params.file))
+    }
+})
+
+// default library path for ease of use
+app.use('/library', express.static(path.join(__dirname, defaultLibrary.path)))
 
 app.listen(3000, () => console.log('listening on port 3000'))
