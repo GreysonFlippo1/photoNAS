fbaa5bb374101e01575f13df9d4ab84cde3819bc
GreysonFlippo1 <greysonflippo@gmail.com>
2024-09-07 17:29:55 -0700

    added simple webpack app

---
+++ web-client/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = 
+  {mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
+
