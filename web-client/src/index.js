e4e458e2d4f6168d03b2a439a5a9fc2989637bb1
GreysonFlippo1 <51556136+GreysonFlippo1@users.noreply.github.com>
2024-09-14 14:43:38 -0700

    added style sheet

--- web-client/src/index.js
+++ web-client/src/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Home } from './home'
+import "./styles.css";
 
 // Render your React component instead
 const root = createRoot(document.getElementById('root'))
