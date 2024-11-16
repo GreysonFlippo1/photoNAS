cea0549193a2a9548404e327509c67fac41a27e2
GreysonFlippo1 <greysonflippo@gmail.com>
2024-11-16 15:56:34 -0800

    fixed modal on smaller displays

--- web-client/src/tabs/create_library_modal.js
+++ web-client/src/tabs/create_library_modal.js
@@ -56,7 +56,6 @@ export const CreateLibraryModal = (props) => {
     }, [setLibraryOptions])
 
     return <div className='modalContainer'>
-        <div className='modalBG'></div>
         <div className='modalForm'>
             {
                 libraryOptions ? <>
