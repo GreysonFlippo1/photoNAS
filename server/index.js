6827b24b614a22164c9e48db68bb60d7f6416ca8
GreysonFlippo1 <greysonflippo@gmail.com>
2025-08-19 21:59:00 -0700

    added eslint config

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const {getConfig, setHeaders} = require('./utils/configurator')
+const {getConfig} = require('./utils/configurator')
 
 app.use(express.json())
 
